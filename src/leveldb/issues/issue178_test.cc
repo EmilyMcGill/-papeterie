28d85235b4ea9d9738c4326d414f0f8934e4e219
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2020-11-19 13:19:01 +0000

    Fix filtering

--- src/leveldb/issues/issue178_test.cc
+++ src/leveldb/issues/issue178_test.cc
@@ -65,4 +65,28 @@ TEST(Issue178, Test) {
   // compact database
   std::string start_key = Key1(0);
   std::string end_key = Key1(kNumKeys - 1);
-  leveldb::Slice lea
+  leveldb::Slice least(start_key.data(), start_key.size());
+  leveldb::Slice greatest(end_key.data(), end_key.size());
+
+  // commenting out the line below causes the example to work correctly
+  db->CompactRange(&least, &greatest);
+
+  // count the keys
+  leveldb::Iterator* iter = db->NewIterator(leveldb::ReadOptions());
+  size_t num_keys = 0;
+  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+    num_keys++;
+  }
+  delete iter;
+  ASSERT_EQ(kNumKeys, num_keys) << "Bad number of keys";
+
+  // close database
+  delete db;
+  DestroyDB(dbpath, leveldb::Options());
+}
+
+}  // anonymous namespace
+
+int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
