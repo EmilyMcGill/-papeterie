0af5aff9292fe2680fc9ad9edd1e0f1a3610d28e
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2020-11-21 09:53:58 +0000

    Added .idea

--- src/leveldb/port/port_win.h
+++ src/leveldb/port/port_win.h
@@ -141,3 +141,35 @@ inline bool Snappy_Compress(const char* input, size_t length,
   size_t outlen;
   snappy::RawCompress(input, length, &(*output)[0], &outlen);
   output->resize(outlen);
+  return true;
+#endif
+
+  return false;
+}
+
+inline bool Snappy_GetUncompressedLength(const char* input, size_t length,
+                                         size_t* result) {
+#ifdef SNAPPY
+  return snappy::GetUncompressedLength(input, length, result);
+#else
+  return false;
+#endif
+}
+
+inline bool Snappy_Uncompress(const char* input, size_t length,
+                              char* output) {
+#ifdef SNAPPY
+  return snappy::RawUncompress(input, length, output);
+#else
+  return false;
+#endif
+}
+
+inline bool GetHeapProfile(void (*func)(void*, const char*, int), void* arg) {
+  return false;
+}
+
+}
+}
+
+#endif  // STORAGE_LEVELDB_PORT_PORT_WIN_H_
