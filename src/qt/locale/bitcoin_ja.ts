c34e620e53a1f9b56ce8d3cecaf41e4e870d450e
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2021-09-25 15:16:46 +0000

    Version 2.0.1

---
+++ src/qt/locale/bitcoin_ja.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.1">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About Abjcoin</source>
+        <translation>ブラックコインについて</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;Abjcoin&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;ブラックコイン&lt;/b&gt;バージョン</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2014 The Abjcoin developers</source>
+        <translation>コピーライト © 2009-2014 The Bitcoin developers
+コピーライト © 2012-2014 The NovaCoin developers
+コピーライト © 2014 The Abjcoin developers</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.</source>
+        <translation>
+これは実験的なソフトウェアです。
+
+MIT/X11 ソフトウェア ライセンスの下で配布されています。詳しくは添付の COPYING ファイルやhttp://www.opensource.org/licenses/mit-license.php を参照してください。
+
+この製品は OpenSSL Toolkit (http://www.openssl.org/) に用いられる  Eric Young (eay@cryptsoft.com)  が開発した暗号化ソフトウェアと Thomas Bernard が開発した UPnP ソフトウェアを含んでいます。</translation>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>アドレス帳</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <sour
