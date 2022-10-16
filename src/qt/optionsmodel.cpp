19e8d9c95d7373e513a3e579a17ac290aae67a69
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2022-10-16 22:15:17 +0000

    Fix positioning

---
+++ src/qt/optionsmodel.cpp
@@ -0,0 +1,120 @@
+#include "optionsmodel.h"
+#include "bitcoinunits.h"
+#include <QSettings>
+
+#include "init.h"
+#include "walletdb.h"
+#include "guiutil.h"
+
+OptionsModel::OptionsModel(QObject *parent) :
+    QAbstractListModel(parent)
+{
+    Init();
+}
+
+bool static ApplyProxySettings()
+{
+    QSettings settings;
+    CService addrProxy(settings.value("addrProxy", "127.0.0.1:9050").toString().toStdString());
+    int nSocksVersion(settings.value("nSocksVersion", 5).toInt());
+    if (!settings.value("fUseProxy", false).toBool()) {
+        addrProxy = CService();
+        nSocksVersion = 0;
+        return false;
+    }
+    if (nSocksVersion && !addrProxy.IsValid())
+        return false;
+    if (!IsLimited(NET_IPV4))
+        SetProxy(NET_IPV4, addrProxy, nSocksVersion);
+    if (nSocksVersion > 4) {
+        if (!IsLimited(NET_IPV6))
+            SetProxy(NET_IPV6, addrProxy, nSocksVersion);
+        SetNameProxy(addrProxy, nSocksVersion);
+    }
+    return true;
+}
+
+void OptionsModel::Init()
+{
+    QSettings settings;
+
+    // These are Qt-only settings:
+    nDisplayUnit = settings.value("nDisplayUnit", BitcoinUnits::BTC).toInt();
+    bDisplayAddresses = settings.value("bDisplayAddresses", false).toBool();
+    fMinimizeToTray = settings.value("fMinimizeToTray", false).toBool();
+    fMinimizeOnClose = settings.value("fMinimizeOnClose", false).toBool();
+    fCoinControlFeatures = settings.value("fCoinControlFeatures", false).toBool();
+    nTransactionFee = settings.value("nTransactionFee").toLongLong();
+    nReserveBalance = settings.value("nReserveBalance").toLongLong();
+    language = settings.value("language", "").toString();
+
+    // These are shared with core Bitcoin; we want
+    // command-line options to override the GUI settings:
+    if (settings.contains("fUseUPnP"))
+        SoftSetBoolArg("-upnp", settings.value("fUseUPnP").toBool());
+    if (settings.contains("addrProxy") && settings.value("fUseProxy").toBool())
+        SoftSetArg("-proxy", settings.value("addrProxy").toString().toStdString());
+    if (settings.contains("nSocksVersion") && settings.value("fUseProxy").toBool())
+        SoftSetArg("-socks", settings.value("nSocksVersion").toString().toStdString());
+    if (!language.isEmpty())
+        SoftSetArg("-lang", language.toStdString());
+}
+
+int OptionsModel::rowCount(const QModelIndex & parent) const
+{
+    return OptionIDRowCount;
+}
+
+QVariant OptionsModel::data(const QModelIndex & index, int role) const
+{
+    if(role == Qt::EditRole)
+    {
+        QSettings settings;
+        switch(index.row())
+        {
+        case StartAtStartup:
+            return QVariant(GUIUtil::GetStartOnSystemStartup());
+        case MinimizeToTray:
+            return QVariant(fMinimizeToTray);
+        case MapPortUPnP:
+            return settings.value("fUseUPnP", GetBoolArg("-upnp", true));
+        case MinimizeOnClose:
+            return QVariant(fMinimizeOnClose);
+        case ProxyUse:
+            return settings.value("fUseProxy", false);
+        case ProxyIP: {
+            proxyType proxy;
+            if (GetProxy(NET_IPV4, proxy))
+                return QVariant(QString::fromStdString(proxy.first.ToStringIP()));
+            else
+                return QVariant(QString::fromStdString("127.0.0.1"));
+        }
+        case ProxyPort: {
+            proxyType proxy;
+            if (GetProxy(NET_IPV4, proxy))
+                return QVariant(proxy.first.GetPort());
+            else
+                return QVariant(9050);
+        }
+        case ProxySocksVersion:
+            return settings.value("nSocksVersion", 5);
+        case Fee:
+            return QVariant((qint64) nTransactionFee);
+        case ReserveBalance:
+            return QVariant((qint64) nReserveBalance);
+        case DisplayUnit:
+            return QVariant(nDisplayUnit);
+        case DisplayAddresses:
+            return QVariant(bDisplayAddresses);
+        case Language:
+            return settings.value("language", "");
+        case CoinControlFeatures:
+            return QVariant(fCoinControlFeatures);
+        default:
+            return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+bool OptionsModel::setData(const QModelIndex & index, const QVar
