28ebdd9f5784074edb5070ce786683c5a53d43b8
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2022-10-17 16:39:48 +0000

    Update vim-fugitive plugin

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -73,4 +73,99 @@ void QRCodeDialog::genCode()
                 p++;
             }
         }
-        Q
+        QRcode_free(code);
+
+        ui->lblQRCode->setPixmap(QPixmap::fromImage(myImage).scaled(300, 300));
+
+        ui->outUri->setPlainText(uri);
+    }
+}
+
+QString QRCodeDialog::getURI()
+{
+    QString ret = QString("abjcoin:%1").arg(address);
+    int paramCount = 0;
+
+    ui->outUri->clear();
+
+    if (ui->chkReqPayment->isChecked())
+    {
+        if (ui->lnReqAmount->validate())
+        {
+            // even if we allow a non BTC unit input in lnReqAmount, we generate the URI with BTC as unit (as defined in BIP21)
+            ret += QString("?amount=%1").arg(BitcoinUnits::format(BitcoinUnits::BTC, ui->lnReqAmount->value()));
+            paramCount++;
+        }
+        else
+        {
+            ui->btnSaveAs->setEnabled(false);
+            ui->lblQRCode->setText(tr("The entered amount is invalid, please check."));
+            return QString("");
+        }
+    }
+
+    if (!ui->lnLabel->text().isEmpty())
+    {
+        QString lbl(QUrl::toPercentEncoding(ui->lnLabel->text()));
+        ret += QString("%1label=%2").arg(paramCount == 0 ? "?" : "&").arg(lbl);
+        paramCount++;
+    }
+
+    if (!ui->lnMessage->text().isEmpty())
+    {
+        QString msg(QUrl::toPercentEncoding(ui->lnMessage->text()));
+        ret += QString("%1message=%2").arg(paramCount == 0 ? "?" : "&").arg(msg);
+        paramCount++;
+    }
+
+    // limit URI length to prevent a DoS against the QR-Code dialog
+    if (ret.length() > MAX_URI_LENGTH)
+    {
+        ui->btnSaveAs->setEnabled(false);
+        ui->lblQRCode->setText(tr("Resulting URI too long, try to reduce the text for label / message."));
+        return QString("");
+    }
+
+    ui->btnSaveAs->setEnabled(true);
+    return ret;
+}
+
+void QRCodeDialog::on_lnReqAmount_textChanged()
+{
+    genCode();
+}
+
+void QRCodeDialog::on_lnLabel_textChanged()
+{
+    genCode();
+}
+
+void QRCodeDialog::on_lnMessage_textChanged()
+{
+    genCode();
+}
+
+void QRCodeDialog::on_btnSaveAs_clicked()
+{
+    QString fn = GUIUtil::getSaveFileName(this, tr("Save QR Code"), QString(), tr("PNG Images (*.png)"));
+    if (!fn.isEmpty())
+        myImage.scaled(EXPORT_IMAGE_SIZE, EXPORT_IMAGE_SIZE).save(fn);
+}
+
+void QRCodeDialog::on_chkReqPayment_toggled(bool fChecked)
+{
+    if (!fChecked)
+        // if chkReqPayment is not active, don't display lnReqAmount as invalid
+        ui->lnReqAmount->setValid(true);
+
+    genCode();
+}
+
+void QRCodeDialog::updateDisplayUnit()
+{
+    if (model)
+    {
+        // Update lnReqAmount with the current unit
+        ui->lnReqAmount->setDisplayUnit(model->getDisplayUnit());
+    }
+}
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,11 @@
+
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Abjcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AbjcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
