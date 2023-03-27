bade1ad5c31b289d8d2d87b5ad6ee6cfba4fd563
EmilyMcGill <91940586+EmilyMcGill@users.noreply.github.com>
2023-03-27 17:10:58 +0000

    amend readme

---
+++ src/wallet.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_WALLET_H
+#define BITCOIN_WALLET_H
+
+#include <string>
+#include <vector>
+
+#include <stdlib.h>
+
+#include "main.h"
+#include "key.h"
+#include "keystore.h"
+#include "script.h"
+#include "ui_interface.h"
+#include "util.h"
+#include "walletdb.h"
+
+extern bool fWalletUnlockStakingOnly;
+extern bool fConfChange;
+class CAccountingEntry;
+class CWalletTx;
+class CReserveKey;
+class COutput;
+class CCoinControl;
+
+/** (client) version numbers for particular wallet features */
+enum WalletFeature
+{
+    FEATURE_BASE = 10500, // the earliest version new wallets supports (only useful for getinfo's clientversion output)
+
+    FEATURE_WALLETCRYPT = 40000, // wallet encryption
+    FEATURE_COMPRPUBKEY = 60000, // compressed public keys
+
+    FEATURE_LATEST = 60000
+};
+
+/** A key pool entry */
+class CKeyPool
+{
+public:
+    int64_t nTime;
+    CPubKey vchPubKey;
+
+    CKeyPool()
+    {
+        nTime = GetTime();
+    }
+
+    CKeyPool(const CPubKey& vchPubKeyIn)
+    {
+        nTime = GetTime();
+        vchPubKey = vchPubKeyIn;
+    }
+
+    IMPLEMENT_SERIALIZE
+    (
+        if (!(nType & SER_GETHASH))
+            READWRITE(nVersion);
+   
